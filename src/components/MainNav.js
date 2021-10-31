bd21b45c28037621845904c4b8ecf16d68b2b5de
Okikiola Omotosho <omotoshookiki@gmail.com>
2021-10-31 03:12:18 +0100

    fixed scrollcar issues and add the redirects file

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,47 +1,50 @@
+import React from "react";
+import { Link } from "react-router-dom";
 
-
-import React from 'react'
-
-import {ReactComponent as Storelogo} from "../assets/icons/storelogo-icon.svg"
-import {ReactComponent as Homeicon} from "../assets/icons/home-icon1.svg"
-import {ReactComponent as Discount} from "../assets/icons/discount-icon.svg"
-import {ReactComponent as Dashboard} from "../assets/icons/dashboard-icon.svg"
-import {ReactComponent as Message} from "../assets/icons/message-icon.svg"
-import {ReactComponent as Notification} from "../assets/icons/notification-icon.svg"
-import {ReactComponent as Settings} from "../assets/icons/settings-icon.svg"
-import {ReactComponent as Logout} from "../assets/icons/logout-icon.svg"
+import { ReactComponent as Storelogo } from "../assets/icons/storelogo-icon.svg";
+import { ReactComponent as Homeicon } from "../assets/icons/home-icon1.svg";
+import { ReactComponent as Discount } from "../assets/icons/discount-icon.svg";
+import { ReactComponent as Dashboard } from "../assets/icons/dashboard-icon.svg";
+import { ReactComponent as Message } from "../assets/icons/message-icon.svg";
+import { ReactComponent as Notification } from "../assets/icons/notification-icon.svg";
+import { ReactComponent as Settings } from "../assets/icons/settings-icon.svg";
+import { ReactComponent as Logout } from "../assets/icons/logout-icon.svg";
 
 function MainNav() {
-    return (
-        <div className="navbar">
+  return (
+    <div className="navbar">
+      <div className="navbar__icon navbar__iconbox--1">
+        <Storelogo className="icon" />
+      </div>
 
-            <div className="navbar__icon navbar__iconbox--1">
-                <Storelogo className="icon"/>
-            </div>
-            <div className="navbar__icon navbar__iconbox--2">
-                <Homeicon className="icon" />
-            </div>
-            <div className="navbar__icon navbar__iconbox--3">
-                <Discount className="icon"/>
-            </div>
-            <div className="navbar__icon navbar__iconbox--4">
-                <Dashboard className="icon"/>
-            </div>
-            <div className="navbar__icon navbar__iconbox--3">
-                <Message className="icon"/>
-            </div>
-            <div className="navbar__icon navbar__iconbox--4">
-                <Notification className="icon"/>
-            </div>
-            <div className="navbar__icon navbar__iconbox--4">
-                <Settings className="icon"/>
-            </div>
-            <div className="navbar__icon navbar__iconbox--4">
-                <Logout className="icon"/>
-            </div>
-            
-        </div>
-    )
+      <div className="navbar__icon navbar__iconbox--2">
+        <Homeicon className="icon" />
+      </div>
+
+      <div className="navbar__icon navbar__iconbox--3">
+        <Discount className="icon" />
+      </div>
+
+      <div className="navbar__icon navbar__iconbox--4">
+        <Dashboard className="icon" />
+      </div>
+
+      <div className="navbar__icon navbar__iconbox--3">
+        <Message className="icon" />
+      </div>
+      <div className="navbar__icon navbar__iconbox--4">
+        <Notification className="icon" />
+      </div>
+
+      <div className="navbar__icon navbar__iconbox--4">
+        <Settings className="icon" />
+      </div>
+
+      <div className="navbar__icon navbar__iconbox--4">
+        <Logout className="icon" />
+      </div>
+    </div>
+  );
 }
 
-export default MainNav
+export default MainNav;
